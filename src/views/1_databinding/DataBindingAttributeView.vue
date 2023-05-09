ea7c97d7bcc451e039feeb2a0ae25809635cdf00
Go YongSu <itgys91@kbet.or.kr>
2023-05-09 17:27:04 +0900

    databinding

---
+++ src/views/1_databinding/DataBindingAttributeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" v-bind:value="userId" readonly />
+    <input type="text" :value="userId" readonly />
+    <br />
+    <img :src="imgSrc" alt="" style="width: 100px; height: auto" />
+    <br />
+    <input type="search" name="" id="" v-model="txt1" />
+    <button :disabled="txt1 === ''">조회</button>
+    <!-- txt1이 비어있으면 true -->
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      userId: 'oOpsDev.Go',
+      imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png',
+      txt1: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
---
+++ src/views/1_databinding/DataBindingSelectView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <select name="" id="" v-model="selectedCity">
+      <option value=""></option>
+      <option value="02">서울</option>
+      <option value="042">대전</option>
+      <option value="051">부산</option>
+    </select>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      selectedCity: '042'
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
