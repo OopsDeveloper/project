05bb373bfb3e08a9ac9051a2385edd5ac58cb6dc
고용수 <ajemxkem@gmail.com>
2023-05-30 17:03:23 +0900

    상태관리(vuex)

--- src/router/index.js
+++ src/router/index.js
@@ -208,6 +208,22 @@ const routes = [
       import(
         /* webpackChunkName: "advanced", webpackPrefetch:true */ '../views/5_advanced/PluginView.vue'
       )
+  },
+  {
+    path: '/vuex/todo',
+    name: 'TodoView',
+    component: () =>
+      import(
+        /* webpackChunkName: "vuex", webpackPrefetch:true */ '../views/6_vuex/TodoView.vue'
+      )
+  },
+  {
+    path: '/vuex/login',
+    name: 'LoginView',
+    component: () =>
+      import(
+        /* webpackChunkName: "vuex", webpackPrefetch:true */ '../views/6_vuex/LoginView.vue'
+      )
   }
 ]
 
